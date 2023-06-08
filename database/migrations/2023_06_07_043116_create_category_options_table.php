355437fc94f8128339ac1a75ce73bbe1294c933f
ringlives <shohidurr49@gmail.com>
2023-06-08 13:11:25 +0600

    Working on general  setting

--- database/migrations/2023_06_07_043116_create_category_options_table.php
+++ database/migrations/2023_06_07_043116_create_category_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class CreateCategoryOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('category_options', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('category_option_name');
             $table->tinyInteger('active')->default(0);
             $table->timestamps();
         });
@@ -29,6 +28,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('category_options');
     }
 }
